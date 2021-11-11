41e70d0a1651d131ae3394e9e9305e9f9985483d
javoxir-akram <akramov.javoxir@yandex.ru>
2021-11-11 19:17:52 +0300

    Следим за временем, необходимым на выполнение вашего кода на Python
    Допустим, вам необходимо узнать, сколько времени занимает выполнение той или иной функции. Используя модуль time, вы можете рассчитать это время.

---
+++ izmerit-vremya.py
@@ -0,0 +1,12 @@
+"""
+Created on Thu Nov 11 18:38:33 2021
+
+@author: Lenovo
+"""
+import time
+stime = time.time()
+for i in range(1,5000):
+    print(i)
+etime = time.time()
+ttime = etime - stime
+print("Vaqt",ttime)
